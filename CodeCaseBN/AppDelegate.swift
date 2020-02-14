8d806ac910a53111d071f364d7d54b268b7772c6
Ahmet Sina Ustem <ahmet.sina@applogist.com>
2020-02-14 21:47:26 +0300

    Initial Commit

---
+++ CodeCaseBN/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CodeCaseBN
+//
+//  Created by AppLogist on 14.02.2020.
+//  Copyright © 2020 AhmetSina. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ CodeCaseBN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeCaseBN
+//
+//  Created by AppLogist on 14.02.2020.
+//  Copyright © 2020 AhmetSina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
